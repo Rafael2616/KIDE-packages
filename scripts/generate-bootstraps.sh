1cc6cc4e257f20ba37fe0bf4053a615e0e2a7064
Rafael <rafaelmoura135790@gmail.com>
2025-10-16 21:58:53 -0300

    fix build?

--- scripts/generate-bootstraps.sh
+++ scripts/generate-bootstraps.sh
@@ -24,7 +24,7 @@ TERMUX_PACKAGE_MANAGERS=("apt" "pacman")
 
 # The repository base urls mapping for package managers.
 declare -A REPO_BASE_URLS=(
-	["apt"]="https://mirror.accum.se/mirror/termux.dev/termux-main/"
+	["apt"]="http://mirror.mephi.ru/termux/termux-main/"
 	["pacman"]="https://service.termux-pacman.dev/main"
 )
 
